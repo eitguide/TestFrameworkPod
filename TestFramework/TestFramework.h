ad72899e1f6846ee6a5dc1e44d82a6945b1715b2
Nguyen Nghia <nghianv@vng.com.vn>
2019-07-13 20:37:02 +0700

    first commit

---
+++ TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Nguyen Van Nghia on 7/13/19.
+//  Copyright © 2019 Nguyen Van Nghia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
---
+++ TestFrameworkExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFrameworkExample
+//
+//  Created by Nguyen Van Nghia on 7/13/19.
+//  Copyright © 2019 Nguyen Van Nghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
